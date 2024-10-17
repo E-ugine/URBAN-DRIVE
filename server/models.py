563f24d4aecc133ed4c3d15958f07d48693357eb
Frankline Were <franklinegift@gmail.com>
2024-10-17 14:45:54 +0300

    define relationships

--- server/models.py
+++ server/models.py
@@ -6,19 +6,21 @@ metadata = MetaData()
 db = SQLAlchemy(metadata=metadata)
 
 
-class Customer(db.Model):
-    __tablename__ = "customers"
+class User(db.Model):
+    __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String, nullable=False)
     last_name = db.Column(db.String, nullable=False)
     email = db.Column(db.String) # email validation
+    hashed_password = db.Column(db.String)
     phone_number = db.Column(db.String)
     address = db.Column(db.String)
     dl_number = db.Column(db.String)
+    bookings = db.relationship('Booking', backref='user', lazy=True) # one user : many bookings relationship
 
     def __repr__(self):
-        return f"<Customer {self.id}, {self.name}>"
+        return f"<Customer {self.id}, {self.first_name}>"
     
 
 class Car(db.Model):
@@ -31,7 +33,8 @@ class Car(db.Model):
     colour = db.Column(db.String)
     license_plate = db.Column(db.String)
     status = db.Column(db.String, default="available") # check on use of default
-    daily_rate = db.Column(db.Float)
+    price_per_day = db.Column(db.Float)
+    bookings = db.relationship('Booking', backref='car', lazy=True) # one car : many bookings relationship
 
     def __repr__(self):
         return f"<Car {self.id}, {self.make}, {self.model}>"
@@ -41,13 +44,24 @@ class Booking(db.Model):
     __tablename__ = "bookings"
 
     id = db.Column(db.Integer, primary_key=True)
-    customer_id = db.Column(db.Integer, db.ForeignKey('customers.id')) # foreign key mapping to customers table
+    customer_id = db.Column(db.Integer, db.ForeignKey('users.id')) # foreign key mapping to customers table
     car_id = db.Column(db.Integer, db.ForeignKey('cars.id')) # foreign key mapping to cars table
     start_date = db.Column(db.DateTime, nullable=False)
     end_date = db.Column(db.DateTime, nullable=False)
     total_cost = db.Column(db.Float)
     status = db.Column(db.String, default="pending") # pending or active
+    payments = db.relationship('Payment', backref='booking', lazy=True)
 
     def __repr__(self):
         return f"<Car {self.id}, {self.customer_id}, {self.car_id}>"
+    
+
+class Payment(db.Model):
+    __tablename__ = "payments"
+
+    id = db.Column(db.Integer, primary_key=True)
+    booking_id = db.Column(db.Integer, db.ForeignKey('bookings.id'))
+    total_cost = db.Column(db.Float)
+    payment_date = db.Column(db.DateTime, nullable=False)
+    status = db.Column(db.String, default="pending") # pending or active
     
