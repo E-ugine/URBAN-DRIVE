5cdcf26e1a4916e6acb6e1b7b24969cbed6e4346
EugineAgolla <agollaeugine@gmail.com>
2024-10-19 14:03:56 +0300

    styles booking form

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,13 +3,11 @@ import Hero from './Hero';
 import Services from './Services';
 import AvailableCars from './AvailableCars';
 import '../styles/home.css';
-import BookingForm from './BookingForm';
 
 function Home() {
   return (
     <div>
       <Hero />
-      <BookingForm/>
       <div className="welcome-section">
         <h2>Welcome to UrbanDrive</h2>
         <p>Your journey starts here. Find the perfect car for your adventure.</p>
