bf3331c156c4f2b1cfaa146e2a36e5ffa93e6ef2
EugineAgolla <agollaeugine@gmail.com>
2024-10-20 22:30:23 +0300

    styles homepage

--- client/src/components/Helmet.js
+++ client/src/components/Helmet.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Helmet = (props) => {
-  document.title = "Urban Drive - " + props.title;
+  document.title = "Urban Drive - " ;
   return <div className="w-100">{props.children}</div>;
 };
 
