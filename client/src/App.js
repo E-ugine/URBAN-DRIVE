8110d6c6b240b21b0123d88dada1fcee0f9d63f0
roynguithi <roy.nguithi@students.moringaschool.com>
2024-10-21 08:43:52 +0300

    SignUp Added

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { UserProvider } from './context/UserContext'; 
+import { UserProvider } from './context/UserContext';
+import SignUp from "./components/SignUp"; 
 import Home from "./components/Home";
 import About from "./components/About";
 import Cars from "./components/Cars";
@@ -13,19 +14,21 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
 
+
 function App() {
   return (
     <UserProvider>
       <Router>
         <Navbar />
         <Routes>
+          <Route path="/signup"element={<SignUp />}  />
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/services" element={<Services />} />
           <Route path="/cars" element={<Cars />} />
           <Route path="/booking" element={<BookingForm />} />
           <Route path="/cars/:id" element={<CarsDetail />} />
-          <Route path="/contact" element={<Contact />} /> 
+          <Route path="/contact" element={<Contact />} />
         </Routes>
         <Footer />
       </Router>
